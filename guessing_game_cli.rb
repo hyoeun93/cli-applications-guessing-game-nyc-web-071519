80559c945fad0625744b41753219ed9c38042bfe
HyoEun Lee <hyoeun93@gmail.com>
2019-07-10 02:22:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,9 @@
 # Code your solution here!
 def run_guessing_game
   
-	roll = 1+rand(6)
-	user_input = gets.chomp.to_s
+	roll = 1 + rand(6)
+	user_input = gets.chomp
+	
     if user_input == roll 
     	return "You guessed the correct number!"
     elsif user_input != roll
