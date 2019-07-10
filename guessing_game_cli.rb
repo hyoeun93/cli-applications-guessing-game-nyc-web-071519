727a8fc7dcdf50e0bbbcdd756e571664ddb6ff9b
HyoEun Lee <hyoeun93@gmail.com>
2019-07-10 02:13:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game(num)
   if num === n 
     puts "You guessed the correct number!"
     
-    if num !== n 
+    if !num == n 
       puts "Sorry! The computer guessed n."
       
     else if num === "exit"
