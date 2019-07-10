6b3334a037709d941b262bece186e6ccb4c7532d
HyoEun Lee <hyoeun93@gmail.com>
2019-07-10 02:12:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,4 +1,12 @@
 # Code your solution here!
-def run_guessing_game
+def run_guessing_game(num)
   rand(1...6)
+  if num === n 
+    puts "You guessed the correct number!"
+    
+    if num !== n 
+      puts "Sorry! The computer guessed n."
+      
+    else if num === "exit"
+      puts "Goodbye!"
 end 
